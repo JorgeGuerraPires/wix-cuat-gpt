3d94d9631f73e25632fdc70d41788b9c2b68481e
Jorge Guerra Pires <53444676+JorgeGuerraPires@users.noreply.github.com>
2023-04-01 14:01:52 -0300

    Update masterPage.js

--- masterPage.js
+++ masterPage.js
@@ -1,21 +1,26 @@
 //Importando do backend
 import { sendChatMessage } from "backend/wix-chat";
-import { detectar_palavras_chaves } from "backend/util.jsw";
+import { detectar_palavras_chaves, acessar_banco } from "backend/util.jsw";
 
 //Palavras-chaves e suas respetivas respostas
-const palavraschaves = [{
-    palavras: ["JavaScript"],
-    resposta: "Eu ensino JavaScript. Contrate meus serviços em: http://bit.ly/3KnI9jV"
-}]
+/**Temos um vetor de objetos. 
+ * Cada objeto tem um vetor de palavras chaves, e resposta para essa palavras-chaves 
+ * */
 
-$w.onReady(function () { //executa sempre que tudo está pronto no frontend
+    $w.onReady(async function () { //executa sempre que tudo está pronto no frontend
 
-    $w('#wixChat').onMessageSent(async (mensage) => { //executa somente quando uma mensagem é enviada
-   
-        if (await detectar_palavras_chaves(mensage.payload.text, palavraschaves[0].palavras))
-            sendChatMessage(palavraschaves[0].resposta, mensage.channelId);
-        else
-            sendChatMessage("Infelizmente, não posso te ajudar", mensage.channelId);
+        const palavraschaves = await acessar_banco();
+
+        $w('#wixChat').onMessageSent(async (mensage) => { //executa somente quando uma mensagem é enviada        
+
+            //Note o await. 
+            //Isso foi necessário para evitar que o teste fosee feito na promessa, que será sempre true
+            if (await detectar_palavras_chaves(mensage.payload.text, palavraschaves[0].palavras))
+                sendChatMessage(palavraschaves[0].resposta, mensage.channelId); //palavra-chave achada
+            else
+                //palavra-chave não achada
+                sendChatMessage("Infelizmente, não posso te ajudar", mensage.channelId);
+        })
+
     })
-
-});
+}
